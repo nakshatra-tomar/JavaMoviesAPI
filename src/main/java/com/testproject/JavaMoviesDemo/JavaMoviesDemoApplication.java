420b6da32595c36fc3c9476b821fc07138d6bf57
Nakshatra Tomar <tomarnakshatra@gmail.com>
2024-05-14 18:43:11 +0530

    Init

---
+++ src/main/java/com/testproject/JavaMoviesDemo/JavaMoviesDemoApplication.java
@@ -0,0 +1,20 @@
+package com.testproject.JavaMoviesDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JavaMoviesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMoviesDemoApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String apiRoot(){
+		return "Hello, world";
+	}
+}
---
+++ src/test/java/com/testproject/JavaMoviesDemo/JavaMoviesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testproject.JavaMoviesDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMoviesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
