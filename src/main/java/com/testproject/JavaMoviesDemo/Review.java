9bfe730ca8350a75166e6826b6cfc324df17975f
Nakshatra Tomar <tomarnakshatra@gmail.com>
2024-06-06 23:25:58 +0530

    embedded review class

--- src/main/java/com/testproject/JavaMoviesDemo/Review.java
+++ src/main/java/com/testproject/JavaMoviesDemo/Review.java
@@ -8,28 +8,16 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.List;
-
-
-@Document(collection = "movies")
+@Document(collection = "reviews")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Movie {
+
+public class Review {
 
     @Id
     private ObjectId id;
-    private String imdbID;
-    private String title;
+    private String body;
 
-    private String releaseDate;
 
-    private String trailerLink;
-    private String poster;
-
-    private List<String> genres;
-
-    private List<String> backdrops;
-
-    private List<Review> reviewIds;
 }
