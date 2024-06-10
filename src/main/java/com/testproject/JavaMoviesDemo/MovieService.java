316b9437eae677c2f3104b4c14cf940eaf5669ad
nakshatra-tomar <tomarnakshatra@gmail.com>
2024-06-10 12:05:29 +0530

    added to controller and MovieService

--- src/main/java/com/testproject/JavaMoviesDemo/MovieService.java
+++ src/main/java/com/testproject/JavaMoviesDemo/MovieService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -15,5 +16,9 @@ public class MovieService {
 
         return movieRepository.findAll();
 
+    }
+
+    public Optional<Movie> singleMovie(Object Id) {
+        return movieRepository.findById(id);
     }
 }
