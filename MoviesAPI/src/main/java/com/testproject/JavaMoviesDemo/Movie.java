e95bd82417c738d52440b3c823674dfa8f491245
nakshatra-tomar <tomarnakshatra@gmail.com>
2024-07-14 16:55:32 +0530

    refactoring and client setup

--- MoviesAPI/src/main/java/com/testproject/JavaMoviesDemo/Movie.java
+++ MoviesAPI/src/main/java/com/testproject/JavaMoviesDemo/Movie.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
 
@@ -14,7 +15,6 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Movie {
-
     @Id
     private ObjectId id;
     private String imdbId;
@@ -22,7 +22,18 @@ public class Movie {
     private String releaseDate;
     private String trailerLink;
     private String poster;
-    private List<String> genres;
     private List<String> backdrops;
-    private List<Review> reviewIds;
+    private List<String> genres;
+    @DocumentReference
+    private List<Review> reviews;
+
+    public Movie(String imdbId, String title, String releaseDate, String trailerLink, String poster, List<String> backdrops, List<String> genres) {
+        this.imdbId = imdbId;
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.trailerLink = trailerLink;
+        this.poster = poster;
+        this.backdrops = backdrops;
+        this.genres = genres;
+    }
 }
